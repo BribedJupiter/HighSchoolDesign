b3d372ec02a863d07c0062556a678370349e8c2f
Jack <jgb1017@gmail.com>
2022-09-18 04:08:07 -0500

    Added a lot of displays to the game window. It can also load images now

--- main.py
+++ main.py
@@ -7,14 +7,64 @@ Last Modified: 9/17/22 by Jack
 '''
 
 # Imports our libraries
-import tkinter as tk
+from curses.ascii import CAN
+import tkinter as tk # For the GUI
+from PIL import ImageTk, Image # For displaying images - not sure if this is the best library to go with
+import glob # For reading file paths, not sure if should go with this or not
+import random # For random numbers 
 
-# Define the other windows
+# Setup our image library - not sure if this is the best spot to do this or if it matters
+img = []
+img_folder = 'Images'
+for filename in glob.iglob(f"{img_folder}\*"):
+    img.append(filename)
+    print (filename)
+print (img) # For debugging purposes - not sure why there are two backslashes in the names - "Images\\jupiter1.jpg"
+
+# Define the other windows, we will just call them as a function whenever we need to open the window. 
 def game():
+    # Setup the game window
     global game_window
     game_window = tk.Toplevel()
     game_window.title("CosmoGuessr")
     game_window.geometry("400x400")
+    game_window.rowconfigure([0,1,2,3], minsize=100, weight=1) # Setup our grid
+    game_window.columnconfigure([0,1,2], minsize=100, weight=1)
+
+    # Setup the round number
+    round = 0
+    round_display = tk.Label(master=game_window, text=("Round: " + str(round)))
+    round_display.grid(row=0, column=0, sticky="nw")
+
+    # Setup the score card
+    score = 0
+    score_display = tk.Label(master=game_window, text=("Score: " + str(score)))
+    score_display.grid(row=0, column=1, sticky="n")
+
+    # Setup the hot streak
+    streak = 0
+    streak_display = tk.Label(master=game_window, text=("Hot Streak: " + str(streak)))
+    streak_display.grid(row=0, column=3, sticky="ne")
+
+    # Setup the Image
+    global pic # Needs to be a global variable otherwise it won't display
+    pic = ImageTk.PhotoImage(Image.open(random.choice(img)))
+    img_display = tk.Label(game_window, image=pic)
+    img_display.grid(row=1, column=1, sticky="nsew")
+
+    # Setup the guessr entry form
+    guess_lbl = tk.Label(master=game_window, text="Enter your guess:")
+    guess_lbl.grid(row=2, column=0, sticky="s")
+    guess_entry = tk.Entry(master=game_window, width=50)
+    guess_entry.grid(row=2, column=1, sticky="s")
+
+    
+
+def leaderboard():
+    global leaderboard_window
+    leaderboard_window = tk.Toplevel()
+    leaderboard_window.title("Leaderboard")
+    leaderboard_window.geometry("400x400")
 
 # Creates the root window with a set title and a set height / width
 root = tk.Tk()
@@ -25,6 +75,8 @@ root.geometry("400x400")
 title_lbl = tk.Label(text="Welcome to CosmoGuessr", bg="black", fg="white", pady=10).pack() #bg = background color, fg = foreground color
 
 begin_btn = tk.Button(text="Begin", bg="black",fg="gold", pady=10, command=game).pack() # command = function to call, pady = vertical space, .pack() tells it to put it on the screen
+
+leaderboard_btn = tk.Button(text="Leaderboard",bg="black",fg="gold",pady=10, command=leaderboard).pack()
 
 # Opens the window
 root.mainloop()
